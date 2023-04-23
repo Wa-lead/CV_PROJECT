2eacb155c80ff07fac7dc3b97055cb15b5980b15
Waleed <81301826+Wa-lead@users.noreply.github.com>
2023-04-23 05:23:52 +0300

    first

---
+++ README.md
@@ -0,0 +1,6 @@
+# dependencies ( for macos ) -- make sure tensorflow is compatiable
+> pip install -r requirements.txt
+
+# install the package chemception for global acess
+> pip install -e .
+
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='chemception',
+      version='0.1.0',
+      description='CV project chemception',
+      license='MIT',
+      packages=['chemception'],
+      zip_safe=False)
